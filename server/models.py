bec5188b983cef3f5a8c43ec5355aa44872b2f80
rcblake <127946751+rcblake@users.noreply.github.com>
2023-06-13 19:37:52 -0700

    just added Enum for Enclosure

--- server/models.py
+++ server/models.py
@@ -1,23 +1,39 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from enum import Enum
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
+metadata = MetaData(
+    naming_convention={
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    }
+)
 
 db = SQLAlchemy(metadata=metadata)
 
+
 class Zookeeper(db.Model):
-    __tablename__ = 'zookeepers'
+    __tablename__ = "zookeepers"
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    birthday = db.Column(db.DateTime)
+
+    animals = db.relationship("Animal", backref="zookeeper")
+
 
 class Enclosure(db.Model):
-    __tablename__ = 'enclosures'
+    __tablename__ = "enclosures"
 
     id = db.Column(db.Integer, primary_key=True)
+    enviroment = db.Column(Enum("grass", "sand", "water"))
+
 
 class Animal(db.Model):
-    __tablename__ = 'animals'
+    __tablename__ = "animals"
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    species = db.Column(db.String)
+
+    zookeeper_id = db.Column(db.Integer, db.ForeignKey("zookeepers.id"))
+    enclosure_id = db.Column(db.Integer, db.ForeignKey("enclosures.id"))
